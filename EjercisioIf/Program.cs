0f686f6a5856afae4552e22b9897fba0282a4f27
Josiel Castillo <josielcastillov@gmail.com>
2025-04-10 21:20:37 -0600

    Agregar archivos de proyecto.

---
+++ EjercisioIf/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercisioIf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Definicion variables
+            int numero;
+
+            //Entrada
+            Console.WriteLine("Escriba un numero: ");
+            numero = int.Parse(Console.ReadLine());
+
+            //Proceso
+            numero = numero + numero;
+
+            //Salida
+            Console.WriteLine("La Suma del numero es: " + numero);
+
+            Console.ReadKey();
+        }
+    }
+}
